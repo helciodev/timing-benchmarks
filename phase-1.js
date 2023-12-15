c2821a90211b09d22034ae050e6a41dc847ddc16
helcio <h.andre@giap.pl>
2023-12-15 11:07:11 +0100

    Add gitignore Implemenet addNums and addManyNums functions Install dev depencies

--- phase-1.js
+++ phase-1.js
@@ -1,16 +1,21 @@
 // Adds up positive integers from 1-n
 function addNums(n) {
-  // Fill this in
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
 
+  return sum;
 }
-
 
 // Adds up values of addNums(1) through addNums(n)
 function addManyNums(n) {
-  // Fill this in
+  let result = 0;
 
+  for (let i = 1; i <= n; i++) {
+    result += addNums(i);
+  }
+  return result;
 }
-
-
 
 module.exports = [addNums, addManyNums];
